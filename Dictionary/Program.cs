1f86da658d60777e99ccbe76f8fb22cfc1816f90
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:35:22 +0200

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Skriv in en mening här :");
+
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            char lowerChar = char.ToLower(c);
+
+            if (charCount.ContainsKey(lowerChar))
+            {
+                charCount[lowerChar]++;
+            }
+            else
+            {
+                charCount[lowerChar] = 1;
+            }
+        }
+        Console.WriteLine("Teckenräkning : ");
+        foreach (var item in charCount)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
